c8cb343151bb7cbaa9ffa91c5a5fdef2f5012827
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-10-20 14:43:21 +0100

    Cambiando puerto app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 res.render('index', {title: 'Express'});
 })
 
-var server = app.listen(8089, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
 var host = server.address().address
 var port = server.address().port
 console.log('Escuchando at http://%s:%s', host, port)
