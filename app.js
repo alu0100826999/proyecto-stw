4b85ced70fb250530837ff1a594bc0ba0bf8840d
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-10-20 11:43:02 +0100

    Añadido node para ejecutr aplicación en Heroku

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+var path = require ('path')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('./html'));
+
+app.get('/', function (req, res) {
+res.render('index', {title: 'Express'});
+})
+
+var server = app.listen(8081, function () {
+var host = server.address().address
+var port = server.address().port
+console.log('Escuchando at http://%s:%s', host, port)
+})
