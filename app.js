9ab7eca3fd2f039f2d79c14eb138f3ebf5727cfc
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-11-03 11:51:35 +0000

    Arreglando app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var path = require ('path');
+var path = require ('path')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -8,12 +8,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('./html'));
 
 app.get('/', function (req, res) {
-res.render('index', {title: 'Express'});
+res.render('index.html', {title: 'Express'});
 })
 
-
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Escuchando at http://%s:%s', host, port);
-});
+var server = app.listen(8080, function () {
+var host = server.address().address
+var port = server.address().port
+console.log('Escuchando at http://%s:%s', host, port)
+})
