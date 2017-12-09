ba74cd1faafe5c9a4cff557f256d015f849653ff
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-12-09 13:43:31 +0000

    Aplicacion corriendo localmente

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var url_mlab = 'mongodb://alu0100826999:a_rivares18@ds135186.mlab.com:35186/usersbh'
 
-mongoose.connect(url_mlab, function(error){
+//mongoose.connect(url_mlab, function(error){
 //mongoose.connect(process.env.MONGOLAB_URI, function(error) {
-//mongoose.connect('mongodb://localhost:27017/usersbh', function(error) {
-//mongoose.connect('mongodb://localhost:27017/usersbh', function(error){
+
+mongoose.connect('mongodb://localhost:27017/usersbh', function(error){
   if (error) {
     throw error;
   } else {
