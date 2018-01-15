c6e1814c0870ea330690b887d24da3f1a0369a85
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2018-01-15 14:27:31 +0000

    Arreglando enlaces y extensiones de ficheros

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //mongoose.connect(url_mlab, function(error){
 
 // Para conectar mediante heroku con la base de datos en mlab:
-//mongoose.connect(process.env.MONGOLAB_URI, function(error) {
+mongoose.connect(process.env.MONGOLAB_URI, function(error) {
 
 // Para conectar localmente con la base de datos local:
-mongoose.connect('mongodb://localhost:27017/usersbh', function(error){
+//mongoose.connect('mongodb://localhost:27017/usersbh', function(error){
   if (error) {
     throw error;
   } else {
